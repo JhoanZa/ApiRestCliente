c9381c2044e223624171ec1eef979b1ab8538dcb
jhoan zambrano osorio <alejo.ax1306@gmail.com>
2021-08-28 18:19:14 -0500

    .

--- Models/Usuario/Usuario.cs
+++ Models/Usuario/Usuario.cs
@@ -14,27 +14,41 @@ namespace ApiRestCliente.Models
         [JsonProperty(PropertyName = "tipoUsuario")]
         public int TipoUsuario { get; set; }
 
+        [Required(ErrorMessage = "El campo --Primer nombre-- es requerido.")]
+        [Display(Name = "Primer nombre:")]
         [JsonProperty(PropertyName = "primerNombre")]
         public String PrimerNombre { get; set; }
 
+        [Display(Name = "Segundo nombre:")]
         [JsonProperty(PropertyName = "segundoNombre")]
         public String SegundoNombre { get; set; }
 
+        [Required(ErrorMessage = "El campo --Primer apellido-- es requerido.")]
+        [Display(Name = "Primer apellido:")]
         [JsonProperty(PropertyName = "primerApellido")]
         public String PrimerApellido { get; set; }
 
+        [Display(Name = "Segundo apellido:")]
         [JsonProperty(PropertyName = "segundoApellido")]
         public String SegundoApellido { get; set; }
 
+        [Required(ErrorMessage = "El campo --Fecha de nacimiento-- es requerido")]
+        [Display(Name = "Fecha de nacimiento:")]
         [JsonProperty(PropertyName = "fechaNacimiento")]
         public DateTime FechaNacimiento { get; set; }
 
+        [Required(ErrorMessage = "El campo --Edad- es requerido.")]
+        [Display(Name = "Edad:")]
         [JsonProperty(PropertyName = "edad")]
         public int Edad { get; set; }
 
+        [Required(ErrorMessage = "El campo --Correo-- es requerido.")]
+        [Display(Name = "Correo:")]
         [JsonProperty(PropertyName = "correo")]
         public String Correo { get; set; }
 
+        [Required(ErrorMessage = "El campo --contraseña-- es requerido.")]
+        [Display(Name = "Contraseña:")]
         [JsonProperty(PropertyName = "contrasena")]
         public String Contrasena { get; set; }
 
