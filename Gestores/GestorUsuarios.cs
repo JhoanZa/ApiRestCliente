a0e82d663839022fa7fabc4036ed322c7b3369be
jhoan zambrano osorio <alejo.ax1306@gmail.com>
2021-08-28 16:34:57 -0500

    Agregar archivos de proyecto.

---
+++ Gestores/GestorUsuarios.cs
@@ -0,0 +1,65 @@
+﻿using ApiRestCliente.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Gestores
+{
+    class GestorUsuarios
+    {
+        private static readonly HttpClient client = new();
+        private static readonly String url = "https://localhost:44324/";
+
+        public  GestorUsuarios()
+        {
+        }
+
+        public static Usuario ConsultarUsuario(String correo)
+        {
+            Uri();
+            var request = client.GetAsync($"api/Usuarios/{correo}").Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var Objeto = JsonConvert.DeserializeObject<Usuario>(resultString);
+                return Objeto;
+            }
+            return null;
+        }
+
+        public static bool VerificarUsuario(String correo)
+        {
+           Usuario c = ConsultarUsuario(correo);
+            if (c == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static void Uri()
+        {
+            if (client.BaseAddress == null)
+            {
+                client.BaseAddress = new Uri(url);            
+            }
+        }
+
+        public static bool RegistrarUsuario(Usuario usuario)
+        {
+            if (!VerificarUsuario(usuario.Correo))
+            {
+                _ = client.PostAsync("api/Usuarios/", usuario, new JsonMediaTypeFormatter()).Result;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Models/Usuario/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestCliente.Models
+{
+    public class Usuario
+    {
+
+        [JsonProperty(PropertyName = "tipoUsuario")]
+        public int TipoUsuario { get; set; }
+
+        [JsonProperty(PropertyName = "primerNombre")]
+        public String PrimerNombre { get; set; }
+
+        [JsonProperty(PropertyName = "segundoNombre")]
+        public String SegundoNombre { get; set; }
+
+        [JsonProperty(PropertyName = "primerApellido")]
+        public String PrimerApellido { get; set; }
+
+        [JsonProperty(PropertyName = "segundoApellido")]
+        public String SegundoApellido { get; set; }
+
+        [JsonProperty(PropertyName = "fechaNacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [JsonProperty(PropertyName = "edad")]
+        public int Edad { get; set; }
+
+        [JsonProperty(PropertyName = "correo")]
+        public String Correo { get; set; }
+
+        [JsonProperty(PropertyName = "contrasena")]
+        public String Contrasena { get; set; }
+
+    }
+}
