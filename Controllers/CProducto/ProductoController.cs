42cfc2d191719ed2b2733f7190cd97cf4fd1d058
jhoan zambrano osorio <alejo.ax1306@gmail.com>
2021-09-16 16:29:09 -0500

    .

--- Controllers/CProducto/ProductoController.cs
+++ Controllers/CProducto/ProductoController.cs
@@ -8,16 +8,29 @@ using System.Threading.Tasks;
 
 namespace ApiRestCliente.Controllers.CProducto
 {
+    /*
+     * Esta clase se encarga de brindar toda la funcionalidad relacionada a los procesos que puede realizar un usuario tales como: Registrarse, Iniciar sesión, Modificar sus datos de residencia
+     * y cambiar ciertos datos personales. 
+     */
     public class ProductoController : Controller 
     {
         public IActionResult Index()
         {
+            /*
+             * En este metodo se carga primero los elementos almacenados en la base de datos relacionados a los productos, se cargan solamente
+             * los que esten relacionados con el usuario mediante su correo.
+            */
             UsuarioController.Datos.CargarProductos(GestorProductos.ConsultarProductoCorreo(UsuarioController.Datos.Usuario.Correo));
             return View(UsuarioController.Datos);
         }
 
         public IActionResult Categoria()
         {
+            /*
+             * Este método se encarga de generar la vista en la cual se selecciona la categoría correspondiente al artículo que se desea registrar. Las categorías son definidas por el 
+             * administrador directamente en la BBDD. No se pueden añadir en este programa, solo se pueden consultar. Primero se consulta si el programa contiene algún registro almacenado
+             * en caso de que no se tenga ninguno, se realiza la consulta en la BBDD usando el gestor designado para este tipo de datos.
+             */
             if (UsuarioController.Datos.Categorias == null || UsuarioController.Datos.Categorias.Count < 1)
             {
                 UsuarioController.Datos.AgregarCategorias(GestorCategoria.ConsultarCategorias());
@@ -26,12 +39,21 @@ namespace ApiRestCliente.Controllers.CProducto
         }
         public IActionResult CategoriaP2()
         {
+            /*
+             * Este método solamente se encarga de carga la vista sobre la cual se debe llenar el formulario de datos sobre el producto a registrar.
+             */
             return View(UsuarioController.Datos);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CategoriaP2(string NombreProducto, string Descripcion, int CantidadDisponible, decimal ValorVenta)
         {
+            /*
+             * Método tipo post mediante el cual se cargan los datos ingresados en el formulario de registro para el producto, se reciben los datos para luego crear el objeto tipo
+             * producto por lo general el producto se crea con identificador 0 en caso de que sea uno nuevo y se inicia en otro número correspondiente al código de un producto
+             * existente en caso de que se desee modificar. El programa valida el código único del producto y si el valor es igual a cero crea uno nuevo, si el valor es distinto de 
+             * cero se modifica los datos del producto que posee el código registrado
+             */
             bool realizado = false;
             UsuarioController.Datos.Producto.CorreoVendedor = UsuarioController.Datos.Usuario.Correo;
             UsuarioController.Datos.Producto.Nombre = NombreProducto;
